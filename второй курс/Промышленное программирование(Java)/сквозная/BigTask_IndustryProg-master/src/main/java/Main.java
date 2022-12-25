293efdf92361dfb2f58a524ac5f0b02b957b0d4b
Ilya Kuzmin <57773285+1ukch@users.noreply.github.com>
2022-12-26 00:28:01 +0300

    Add files via upload

---
+++ второй курс/Промышленное программирование(Java)/сквозная/BigTask_IndustryProg-master/src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+import java.beans.Expression;
+import java.io.*;
+import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fileInputStream = new FileInputStream("__fixtures__/input.txt");
+        Scanner scanner = new Scanner(fileInputStream);
+        PrintWriter printWriter = new PrintWriter(new File("__fixtures__/output.txt"));
+
+        FileInputStream fis = new FileInputStream("__fixtures__/input.txt");
+        byte[] buffer = new byte[fis.available()];
+        fis.read(buffer);
+        ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("__fixtures__/archive.zip"));
+        ZipEntry zipEntry = new ZipEntry("exp.txt");
+        zout.putNextEntry(zipEntry);
+        zout.write(buffer);
+        zout.closeEntry();
+        zout.close();
+
+        String exp = scanner.nextLine();
+        printWriter.close();
+    }
+}
